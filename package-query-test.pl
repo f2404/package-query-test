f348d98ad0c97884feb47ed2eb592750b9acc205
Igor <f2404@yandex.ru>
2015-12-14 16:50:19 +0300

    Add more tests

--- package-query-test.pl
+++ package-query-test.pl
@@ -8,64 +8,90 @@ die "Unable to run $pquery\n" if (! -f $pquery || ! -x $pquery);
 
 my $usage_pattern = 'Usage: package-query \[options\] \[targets \.\.\.\]';
 my $perl_info_pattern = 'core\/perl (\d+\.?)+\-\d+ \(base\)';
+my $dummy_path = '/dummy/path';
 
 my @tests;
 # Test entry structure:
 # SUB - test function, PATTERN - result to check against, INFO - info on test
 
-push @tests, my $test_help_s = {
+push @tests, {
     SUB =>      sub { return qx($pquery -h 2>&1 > /dev/null); },
     PATTERN =>  $usage_pattern,
     INFO =>     'Help info (-h)',
 };
-push @tests, my $test_help_l = {
+push @tests, {
     SUB =>      sub { return qx($pquery --help 2>&1 > /dev/null); },
     PATTERN =>  $usage_pattern,
     INFO =>     'Help info (--help)',
 };
-push @tests, my $test_version = {
+push @tests, {
     SUB =>      sub { return qx($pquery -v); },
     PATTERN =>  'package-query (\d+\.?)+',
     INFO =>     'Version info',
 };
-push @tests, my $test_query = {
+push @tests, {
     SUB =>      sub { return qx($pquery -Q --nocolor); },
     PATTERN =>  $perl_info_pattern,
     INFO =>     'Empty query',
 };
-push @tests, my $test_query_info = {
+push @tests, {
     SUB =>      sub { return qx($pquery -Qi perl --nocolor); },
     PATTERN =>  $perl_info_pattern,
     INFO =>     'Query package info',
 };
-push @tests, my $test_query_search = {
+push @tests, {
     SUB =>      sub { return qx($pquery -Qs perl --nocolor); },
     PATTERN =>  $perl_info_pattern,
     INFO =>     'Query-search package',
 };
-push @tests, my $test_query_native = {
+push @tests, {
     SUB =>      sub { return qx($pquery -Qn --nocolor); },
     PATTERN =>  $perl_info_pattern,
     INFO =>     'Query native packages',
 };
-push @tests, my $test_query_foreign  = {
+push @tests, {
     SUB =>      sub { return qx($pquery -Qm --nocolor); },
     PATTERN =>  'local\/package-query-git (\d+\.?)+',
     INFO =>     'Query foreign packages',
 };
-push @tests, my $test_list_repo = {
+push @tests, {
     SUB =>      sub { return qx($pquery -L --nocolor); },
     PATTERN =>  'core',
     INFO =>     'Repositories list',
 };
-push @tests, my $test_invalid = {
+push @tests, {
+    SUB =>      sub { return qx($pquery -Qn --nocolor -b /var/lib/pacman); },
+    PATTERN =>  $perl_info_pattern,
+    INFO =>     'Database path option (valid path)',
+};
+push @tests, {
+    SUB =>      sub { return qx($pquery -Qn --nocolor -b $dummy_path 2>&1 > /dev/null); },
+    PATTERN =>  'failed to initialize alpm library \(could not find or read directory\)',
+    INFO =>     'Database path option (invalid path)',
+};
+push @tests, {
+    SUB =>      sub { return qx($pquery -Qn --nocolor -c /etc/pacman.conf); },
+    PATTERN =>  $perl_info_pattern,
+    INFO =>     'Config file path option (valid path)',
+};
+push @tests, {
+    SUB =>      sub { return qx($pquery -Qn --nocolor -c $dummy_path 2>&1 > /dev/null); },
+    PATTERN =>  "Unable to open file: $dummy_path",
+    INFO =>     'Config file path option (invalid path)',
+};
+push @tests, {
+    SUB =>      sub { return qx($pquery -Qn -q); },
+    PATTERN =>  '^$',
+    INFO =>     'Quiet (no output)',
+};
+push @tests, {
     SUB =>      sub { return qx($pquery -j 2>&1 > /dev/null); },
     PATTERN =>  "$pquery: invalid option \-\- \'j\'",
     INFO =>     'Invalid option (-j)',
 };
 
 # number of tests to run
-use Test::Simple tests => 10;
+use Test::Simple tests => 15;
 
 for (@tests) {
     ok( $_->{SUB}->() =~ /$_->{PATTERN}/, $_->{INFO} );
